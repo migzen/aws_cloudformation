ee87fbe40704a5c30265648c9020ccd33d2a95b2
glanzmichael25 <glanz.michael.25@gmail.com>
2022-08-10 17:01:28 -0700

    test deploy react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Today Test React App
+          React app test
         </p>
         <a
           className="App-link"
